0314c37becfb07690553ae668910338d34be61cd
anthonymoon2 <104044191+anthonymoon2@users.noreply.github.com>
2022-04-26 17:45:35 -0700

    More stuff

---
+++ Assets/Scripts/Randomizer.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Randomizer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //create something to keep track of which sprites are in which slots
+    // create list of 1-8 sprites
+    // goes through cards and picks random sprite from the list
+    // if 2 of those sprites exist after, reomove that sprite from the list
+    //continue till all slots are filled
+    //reset list for next shuffle
+
+    
+
+    public void 
+    {
+        var list = new List<string> { "one", "two", "three", "four", "five", "six", "seven", "eight" };
+        string RandomSprite = Random.Range(list.Count);
+
+      
+    }
+}
+    
+
+
+
