fbcad5d9d21e7e64cc34c43b51b04185b7a8107d
Ethant3535 <104044004+Ethant3535@users.noreply.github.com>
2022-05-15 18:26:44 -0700

    Just need to fix a few bugs and make the game pretty

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -5,35 +5,142 @@ using UnityEngine.UI;
 
 public class Card : MonoBehaviour
 {
-    public SpriteRenderer SpriteRenderer;
-    public Sprite DefaultSprite;
+    public Sprite frontImage;
+    public Sprite backImage;
+    private bool isFlipped;
+    private bool isFlipping;
+    private Game game;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        game = GameObject.FindObjectOfType<Game>();
     }
-
-    // Update is called once per frame
-    void Update()
+    public void flipToFront()
     {
-        
-    }
-
-    public Sprite seePic(GameObject button)
-    {
-        return SpriteRenderer.sprite;
-    }
-
-    public bool seeIfFlipped(GameObject button)
-    {
-        if (SpriteRenderer.sprite == DefaultSprite)
+        if (isFlipping || isFlipped)
         {
-            return false;
+            return;
         }
-        return true;
+
+        game.addToFlippedCards(this);
+        reveal();
+
+        //Tell the game to add it to the list of flipped cards
+        //Turn it over
     }
-    //What pic it is
-    //Needs to know whether it is flipped
-    //Still need to do on click method so we can know what button/s were clicked on and then be able to pass them into different methods 
+
+    public void flipToBack()
+    {
+        //Turn card back over
+        if (isFlipping || !isFlipped)
+        {
+            return;
+        }
+
+        hide();
+
+    }
+
+
+    public void reveal()
+    {
+        //Turn over all cards
+        isFlipping = true;
+        StartCoroutine(flipToFrontCoroutine());
+
+    }
+
+    public void hide()
+    {
+        isFlipping = true;
+        StartCoroutine(flipToBackCoroutine());
+
+    }
+
+    IEnumerator flipToBackCoroutine()
+    {
+        while (gameObject.transform.rotation.eulerAngles.y > 90)
+        {
+            rotateOneClick(-1, 90);
+            yield return null;
+        }
+        showBackSprite();
+        while (gameObject.transform.rotation.eulerAngles.y > 0)
+        {
+            rotateOneClick(-1, 0);
+            yield return null;
+        }
+        isFlipping = false;
+        isFlipped = true;
+    }
+
+    public void reset()
+    {
+        gameObject.transform.eulerAngles = new Vector3(0, 0, 0);
+        isFlipped = false;
+        isFlipping = false;
+        showBackSprite();
+    }
+
+    IEnumerator flipToFrontCoroutine()
+    {
+        //Rotate 90 degrees
+        //When it is 90 degrees, swap sprite
+        //Keep rotating until 180 degrees
+        while (gameObject.transform.rotation.eulerAngles.y < 90)
+        {
+            rotateOneClick(1,90);
+            yield return null;
+        }
+        showFrontSprite();
+        while (gameObject.transform.rotation.eulerAngles.y < 180)
+        {
+            rotateOneClick(1, 180);
+            yield return null;
+        }
+        isFlipping = false;
+        isFlipped = true;
+    }
+
+    public void rotateOneClick(int direction, float targetAngle)
+    {
+        float newRotation = gameObject.transform.eulerAngles.y+(1*(float) direction);
+        newRotation = keepWithinAngle(targetAngle, newRotation, direction);
+        gameObject.transform.eulerAngles = new Vector3(gameObject.transform.eulerAngles.x, newRotation, gameObject.transform.eulerAngles.z);
+        
+    }
+
+    private float keepWithinAngle(float targetAngle, float currentRotation, int direction)
+    {
+        if (direction == 1)
+        {
+            if (currentRotation > targetAngle)
+            {
+                currentRotation = targetAngle;
+            }
+        }
+
+        else if (direction == -1)
+        {
+            if (currentRotation < targetAngle)
+            {
+                currentRotation = targetAngle;
+            }
+        }
+        return currentRotation;
+    }
+
+    private void showFrontSprite()
+    {
+        gameObject.GetComponent<Image>().sprite = frontImage;
+        gameObject.GetComponent<Image>().transform.localScale = new Vector3(-1, 1, 1);
+    }
+
+    private void showBackSprite()
+    {
+        gameObject.GetComponent<Image>().sprite = backImage;
+        gameObject.GetComponent<Image>().transform.localScale = new Vector3(1, 1, 1);
+    }
+
+
 }
---
+++ Assets/Scripts/Dealer.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Dealer : MonoBehaviour
+{
+    public GameObject CanvasGameObject;
+    public GameObject PanelPrefab4x4;
+    public GameObject CardPrefab;
+
+    public List<Sprite> pictures;
+    private List<Card> cards;
+    private GameObject Panel;
+
+    public void deal()
+    {
+        MakePanel();
+        AttachToCanvas();
+        cards = new List<Card>();
+        for (int i = 0; i < 16; i++)
+        {
+            GameObject card = Instantiate(CardPrefab);
+            card.gameObject.transform.parent = Panel.transform;
+            Card newCard = card.gameObject.GetComponent<Card>();
+            cards.Add(newCard);
+        }
+        assignPicsToCards();
+
+    
+    
+
+    }
+
+    public void assignPicsToCards()
+    {
+        List<Sprite> shuffledPics=makePicsRandomized(pictures);
+        for (int i = 0; i < cards.Count;i++)
+        {
+            cards[i].frontImage = shuffledPics[i];
+        }
+    }
+
+
+    public void reset()
+    {
+
+    }
+
+    private void MakePanel()
+    {
+        Panel = Instantiate(PanelPrefab4x4, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+
+    private void AttachToCanvas()
+    {
+        Panel.gameObject.transform.parent = CanvasGameObject.transform;
+        Panel.gameObject.transform.localPosition = new Vector3(0, 0, 0);
+    }
+
+    private List<Sprite> makePicsRandomized(List<Sprite> spritesInOrder)
+    {
+        System.Random random = new System.Random();
+        return spritesInOrder.OrderBy(a => random.Next()).ToList();
+    }
+}
