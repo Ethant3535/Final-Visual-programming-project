3fd9e95ed65d70e86437cfa275371f1b6540e557
Ethant3535 <104044004+Ethant3535@users.noreply.github.com>
2022-05-19 00:17:38 -0700

    e

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -2,113 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using System.Linq;
 
 public class Game : MonoBehaviour
 {
-    private List<Card> flippedCards;
-    public int matchCounter;
-    public bool isOkToFlip;
-    public Dealer dealer;
+    public List<Image> ImagesOnButtons;
 
+    private int numberOfCardsClicked = 0;
+    private Sprite firstCardSprite;
+    private Sprite secondCardSprite;
+    private int score = 0;
+    public Readouts ReadOuts;
 
-    public void addToFlippedCards(Card card)
+    // Start is called before the first frame update
+    void Start()
     {
-        flippedCards.Add(card);
-        if (flippedCards.Count == 2)
-        {
-            isOkToFlip = false;
-            bool isMatch = checkForMatch();
-            if (isMatch)
-            {
-                matchCounter += 1;
-                flippedCards.Clear();
-                isOkToFlip = true;
-            }
+        
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnCardClicked(int buttonNumber)
+    {
+        if (numberOfCardsClicked == 0)
+        {
+            numberOfCardsClicked = 1;
+            firstCardSprite = ImagesOnButtons[buttonNumber].sprite;
+        }
+        else if (numberOfCardsClicked == 1)
+        {
+            secondCardSprite = ImagesOnButtons[buttonNumber].sprite;
+
+            if (firstCardSprite == secondCardSprite)
+            {
+                print("match!");
+                UpdateMatches(score + 1);
+            }
             else
             {
-                StartCoroutine(waitToFlipBack());
+                print("no match!");
             }
-
-        }
-
-    }
-
-    IEnumerator delayedFlip()
-    {
-        List<Card> allCards = GameObject.FindObjectsOfType<Card>().ToList();
-        allCards.Reverse();
-        for (int i = 0; i < allCards.Count; i++)
-        {
-            allCards[i].reveal();
-            yield return new WaitForSeconds(.25f);
+            numberOfCardsClicked = 0;
         }
     }
 
-    IEnumerator delayedHide()
+    private void UpdateMatches(int newScore)
     {
-        List<Card> allCards = GameObject.FindObjectsOfType<Card>().ToList();
-        allCards.Reverse();
-        for (int i = 0; i < allCards.Count; i++)
-        {
-            allCards[i].hide();
-            yield return new WaitForSeconds(.25f);
-        }
-        isOkToFlip = true;
+        score = newScore;
+        ReadOuts.ShowMatches(score);
+   
     }
 
-    public void revealAllCards()
-    {
-        isOkToFlip = false;
-        StartCoroutine(delayedFlip());
-    }
-
-    public void hideAllCards()
-    {
-        isOkToFlip = false;
-        StartCoroutine(delayedHide());
-    }
-
-    IEnumerator waitToFlipBack()
-    {
-        yield return new WaitForSeconds(2);
-        flippedCards[0].flipToBack();
-        flippedCards[1].flipToBack();
-        flippedCards.Clear();
-        isOkToFlip = true;
-    }
-
-    public bool checkForMatch()
-    {
-        if (flippedCards[0].frontImage == flippedCards[1].frontImage)
-        {
-            print("Match");
-            return true;
-        }
-        print("No Match");
-        return false;
-    }
-
-    public void Reset()
-    {
-        matchCounter = 0;
-        isOkToFlip = true;
-    }
-
-    public bool hasWon()
-    {
-        if (matchCounter == 8)
-        {
-            return true;
-        }
-
-        return false;
-    }
-
-    public void clicked()
-    {
-        dealer.deal();
-    }
 
 }
